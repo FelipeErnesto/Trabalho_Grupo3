69aa5ef5357e267e96a6138426dd4bf52c215218
Felipe Ernesto <felipe.ernesto.campos@usp.br>
2023-04-07 21:52:06 -0300

    Estatísticas

--- intepolação.R
+++ intepolação.R
@@ -20,5 +20,23 @@ for(f in list.files("PLANILHAS LIMPAS"))
         inter <- rbind(inter, linha)
     }
 
-    write.csv(inter, paste0("PLANILHAS INTERPOLADAS/", f))    
+    write.csv(inter, paste0("PLANILHAS INTERPOLADAS/", f), row.names = F)
+    
+    dfmedia <- data.frame(wavelength = inter$wavelength, nadir = NA, "45" = NA, "70" = NA, "-70" = NA, "-45" = NA, check.names=F)
+    dfmediana <- data.frame(wavelength = inter$wavelength, nadir = NA, "45" = NA, "70" = NA, "-70" = NA, "-45" = NA, check.names=F)
+    dfdesvio <- data.frame(wavelength = inter$wavelength, nadir = NA, "45" = NA, "70" = NA, "-70" = NA, "-45" = NA, check.names=F)
+    for(ang in c("nadir", "45", "70", "-45", "-70"))
+    {
+        stat <- inter[names(inter)==ang]
+        media <- apply(stat, 1, mean)
+        mediana <- apply(stat, 1, median)
+        desvio <- apply(stat, 1, sd)
+        
+        dfmedia[ang] <- media
+        dfmediana[ang] <- mediana
+        dfdesvio[ang] <- desvio
+    }
+    write.csv(dfmedia, paste0("ESTATISTICAS/MEDIA/", f), row.names = F)
+    write.csv(dfmediana, paste0("ESTATISTICAS/MEDIANA/", f), row.names = F)
+    write.csv(dfdesvio, paste0("ESTATISTICAS/DESVIO/", f), row.names = F)
 }
